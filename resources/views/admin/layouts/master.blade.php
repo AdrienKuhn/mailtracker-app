a7abbafcf602424b68b65874cf22f44d915fc656
Adrien Kühn <adrien.kuhn@gmx.com>
2015-07-12 14:59:08 +0200

    Fix pages titles.

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Adrien Kühn | @yield('title')</title>
+    <title>Mail Tracker | @yield('title')</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     @section('top_style')
