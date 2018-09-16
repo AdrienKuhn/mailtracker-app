b98cee3cc27d3cf6d53971e0e96e777c1df3d9d8
Adrien Kühn <adrien.kuhn@alayacare.com>
2018-09-16 17:32:45 -0400

    Add EmailCreate command

---
+++ app/Console/Commands/CreateEmail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Email;
+use App\User;
+use Illuminate\Console\Command;
+
+class CreateEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:create {subject : The subject of the email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new mail to track';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = new Email();
+        $email->subject = $this->argument('subject');
+        $email->uniqid = uniqid();
+
+        // Default user
+        $user = User::find(1);
+
+
+        $email = $user->emails()->save($email); // Attach user
+
+        $this->info("Email $email->id successfully created.");
+    }
+}
